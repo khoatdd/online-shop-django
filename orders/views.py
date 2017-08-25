74b19ea29175788e597e9fa06da3f84550e1aa4a
legenddotavn <dangkhoa114@gmail.com>
2017-08-25 17:26:54 +0700

    Add celery to send email asynchronously

--- orders/views.py
+++ orders/views.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from cart.cart import Cart
 from .models import OrderItem
 from .forms import OrderCreateForm
-from cart.cart import Cart
+from .tasks import order_created
 
 # Create your views here.
 
@@ -23,6 +24,8 @@ def order_create(request):
                     quantity=item['quantity'])
             # clear the cart
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html',
                           {'order': order})
     else:
